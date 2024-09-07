f993393d3095dbeb257994b6fe00f3c75ff1d695
E2YunJeong <dbsdbs3440@gmail.com>
2024-09-08 03:51:17 +0900

    240908 불필요한 줄 삭제

--- src/frame.java
+++ src/frame.java
@@ -1,4 +1,4 @@
-import selectPokemon.mainScreenPanel;
+import mainScreen.mainScreenPanel;
 
 import javax.swing.*;
 
@@ -9,11 +9,11 @@ public class frame extends JFrame {
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-    }
+        }
 
     public static void main(String[] args) {
         frame frame = new frame();
-        mainScreenPanel mainScreenPanel = new mainScreenPanel();
+        mainScreenPanel mainScreenPanel = new mainScreenPanel(frame);
         frame.add(mainScreenPanel);
 
         // Frame의 사이즈를 Panel의 사이즈에 맞춰 자동 설정, Panel클래스에서 PreferredSize가 설정되어있아야 함.
