ec62e986be9ceabed9bbcf049341ab81c3c9556f
ikm <rudals466@naver.com>
2024-09-08 10:26:40 +0900

    selectpokemon화면 수정

--- src/selectPokemon/mainScreenPanel.java
+++ src/selectPokemon/mainScreenPanel.java
@@ -20,12 +20,13 @@ public class mainScreenPanel extends JPanel {
         setLayout(new BorderLayout());
 
         //패널 생성
-        LabelPanel labelPanel = new LabelPanel();
         CharacterButtonPanel characterButtonPanel = new CharacterButtonPanel();
         StrengthPanel strengthPanel = new StrengthPanel();
+        ReturnButtonWithLabelPanel returnButtonWithLabelPanel = new ReturnButtonWithLabelPanel(frame);
 
         //메인 패널에 다른 패널들 추가
-        this.add(labelPanel, BorderLayout.NORTH);
+        this.add(returnButtonWithLabelPanel, BorderLayout.NORTH);
+        //this.add(labelPanel, BorderLayout.NORTH);
         this.add(characterButtonPanel, BorderLayout.CENTER);
         this.add(strengthPanel, BorderLayout.SOUTH);
 
