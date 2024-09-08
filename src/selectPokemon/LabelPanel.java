f6ba553b76ee72fb6b6d1b5f93c3840bcc55bec1
ikm <rudals466@naver.com>
2024-09-08 10:36:04 +0900

    글씨 설정 변경

--- src/selectPokemon/LabelPanel.java
+++ src/selectPokemon/LabelPanel.java
@@ -7,7 +7,10 @@ public class LabelPanel extends JPanel {
     public LabelPanel() {
         //내부 패널 생성, 설정
         JPanel labelPanel = new JPanel(new FlowLayout()); // 기본 설정값이 FlowLayout()으로, 작성 생략 가능
-        JLabel label = new JLabel("사용할 포켓몬을 선택하세요");
+        JLabel label = new JLabel("사용할 포켓몬을 선택하세요", JLabel.CENTER);
+        //폰트 설정
+        label.setFont(new Font("Malgun Godhic", Font.PLAIN, 20));
+
         labelPanel.add(label);
 
         //레이블 패널을 현재 패널에 추가
