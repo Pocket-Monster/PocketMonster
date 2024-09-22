b2b8c42cf185aab4cd55c1abb0b2427b80483d77
Leeseonji <sk301057@skuniv.ac.kr>
2024-09-23 04:12:39 +0900

    상점 버튼 추가가

--- src/selectPokemon/MapPanel.java
+++ src/selectPokemon/MapPanel.java
@@ -1,3 +1,43 @@
+
+package selectPokemon;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import funtions.SwitchPanel;
+import storeScreen.StorePanel;
+
+
+public class MapPanel extends JPanel {
+
+    public MapPanel(JFrame frame) {
+
+        // 지도 이미지 로드 (Tiled에서 저장한 PNG 파일 경로)
+        ImageIcon mapIcon = new ImageIcon("src/map/smallMap.png");  // 수정된 파일 경로
+        JLabel mapLabel = new JLabel(mapIcon);
+        // 지도 이미지를 패널에 추가
+        this.setLayout(new BorderLayout());
+        this.add(mapLabel, BorderLayout.CENTER);
+
+        //상점으로 들어가는 임시 버튼
+        JButton inStoreButton=new JButton("상점");
+        inStoreButton.setBounds(350,300,100,50);
+        inStoreButton.setOpaque(false); // 투명 배경으로 설정
+
+        inStoreButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                JPanel switchToStorePanel=new storeScreen.StorePanel(frame);
+                SwitchPanel.switchPanel(frame,switchToStorePanel);
+            }
+        });
+        this.add(inStoreButton, BorderLayout.SOUTH);
+    }
+}
+
+/* 경민님 코
 package selectPokemon;
 
 import javax.swing.*;
@@ -15,3 +55,4 @@ public class MapPanel extends JPanel {
         this.add(mapLabel, BorderLayout.CENTER);
     }
 }
+*/
