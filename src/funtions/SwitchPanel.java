b07e447597e547eba1b0f21f90fe31413197ed8b
Leeseonji <sk301057@skuniv.ac.kr>
2024-09-23 01:45:31 +0900

    원 상태로 복구

--- src/funtions/SwitchPanel.java
+++ src/funtions/SwitchPanel.java
@@ -1,11 +1,10 @@
 package funtions;
 
 import javax.swing.*;
-import java.awt.event.ActionListener;
 
 public class SwitchPanel extends JFrame{
     // 패널을 전환하는 메서드
-    public static ActionListener switchPanel(JFrame frame, JPanel newPanel) {
+    public static void switchPanel(JFrame frame, JPanel newPanel) {
         // 현재 패널 제거
         frame.getContentPane().removeAll();
         // 새로운 패널 추가
@@ -14,6 +13,6 @@ public class SwitchPanel extends JFrame{
         // 화면 갱신
         frame.revalidate();
         frame.repaint();
-        return null;
+
     }
 }
