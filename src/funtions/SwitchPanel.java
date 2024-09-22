d55bf64381ff30990c473e029cfed3b7a550945f
Leeseonji <sk301057@skuniv.ac.kr>
2024-09-23 00:40:31 +0900

    오류 수정

--- src/funtions/SwitchPanel.java
+++ src/funtions/SwitchPanel.java
@@ -1,10 +1,11 @@
 package funtions;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class SwitchPanel extends JFrame{
     // 패널을 전환하는 메서드
-    public static void switchPanel(JFrame frame, JPanel newPanel) {
+    public static ActionListener switchPanel(JFrame frame, JPanel newPanel) {
         // 현재 패널 제거
         frame.getContentPane().removeAll();
         // 새로운 패널 추가
@@ -13,5 +14,6 @@ public class SwitchPanel extends JFrame{
         // 화면 갱신
         frame.revalidate();
         frame.repaint();
+        return null;
     }
 }
---
+++ src/selectPokemon/InStoreButtonPanel.java
@@ -0,0 +1,2 @@
+package selectPokemon;public class InStoreButtonPanel {
+}
